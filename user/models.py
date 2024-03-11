3f5be999c19504c34392825f1b359095a56b88d4
bilal <azbk2004@gmail.com>
2024-03-11 20:59:52 +0600

    first commit

---
+++ user/models.py
@@ -0,0 +1,105 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, phone_number, username, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+
+        user = self.model(
+            phone_number=phone_number,
+            username=username,
+            email=email
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, username, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            phone_number=phone_number,
+            username=username,
+            email=email
+        )
+        user.is_admin = True
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser):
+    username = models.CharField(
+        max_length=123
+    )
+    email = models.EmailField(
+        unique=True
+    )
+    phone_number = models.CharField(
+        max_length=13,
+        unique=True
+    )
+    address = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True
+    )
+    cover = models.ImageField(
+        upload_to='media/cover',
+        blank=True,
+        null=True
+    )
+    created_date = models.DateTimeField(
+        auto_now_add=True
+    )
+    update_date = models.DateTimeField(
+        auto_now=True
+    )
+    is_stuff = models.BooleanField(
+        default=False
+    )
+    is_admin = models.BooleanField(
+        default=False
+    )
+    status = models.PositiveSmallIntegerField(
+        choices=(
+            (1, 'Обычный пользователь'),
+            (2, 'Модератор')
+        ),
+        default=1
+    )
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['username', 'email']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        """Does the user have a specific permission?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        """Does the user have permissions to view the app `app_label`?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        """Is the user a member of staff?"""
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
